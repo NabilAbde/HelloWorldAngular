d087e77d359505dbd1f6d31ab557bafce516b97f
NabilAbde <80818068+NabilAbde@users.noreply.github.com>
2021-07-30 14:29:29 +0100

    creation 2 routes cars et drivers
    Dans app-routing.module.ts
    const routes: Routes = [
    { path: "cars" , component: CarsListComponent},
    { path: "drivers" , component: DriversListComponent}
    
    ];
    et <router-outlet></router-outlet> dans app.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarsListComponent } from './cars-list/cars-list.component';
+import { DriversListComponent } from './drivers-list/drivers-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: "cars" , component: CarsListComponent},
+{ path: "drivers" , component: DriversListComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
