c39c61f7259672cc49699826f6b005cadb67f340
NabilAbde <80818068+NabilAbde@users.noreply.github.com>
2021-08-03 09:06:10 +0100

    value binding exemple

--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cars-list.component.css']
 })
 export class CarsListComponent implements OnInit {
+  // 1er exmple d interpolation
+  // any quand on ne sait pas quel type (string , int)
+mardiMatin:any ="Ils ont l air bien OFF";
+
+// 2 eme exemple ave objet ou propriete
+game : any ={
+title : "Outrun",
+support: "Arcade",
+multi: 128
+
+};
+
+age:number = 13;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getStock(){
+    return 2050;
+  }
+  
+  getWakeUp(){
+    console.log("Silence de cathedrale");
+  }
 }
