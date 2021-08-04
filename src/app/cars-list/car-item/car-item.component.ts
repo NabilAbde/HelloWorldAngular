3ea65244c3eb8b69f68e53b74afc887dd3c35678
NabilAbde <80818068+NabilAbde@users.noreply.github.com>
2021-08-04 08:43:00 +0100

    tp reservation
    action sur le bouton click avec disable et message

--- src/app/cars-list/car-item/car-item.component.ts
+++ src/app/cars-list/car-item/car-item.component.ts
@@ -7,10 +7,17 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CarItemComponent implements OnInit {
   @Input() vehicule:any
-
+  isNotAvailable : boolean = false;
+  bookingMsg:string="Reservez ce véhicule"
   constructor() { }
 
   ngOnInit(): void {
   }
+  onClickButton(){
+    
+    this.isNotAvailable = true; // ou !this.isNotAvailable
+    this.bookingMsg="Bonne route avec Drive-X";
+        }
+ 
 
 }
