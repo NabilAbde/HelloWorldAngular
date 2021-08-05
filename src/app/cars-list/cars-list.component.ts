3edfb52d20a67fa7517e1594a08574629547b2ef
NabilAbde <80818068+NabilAbde@users.noreply.github.com>
2021-08-05 15:05:26 +0100

    Lifting et utilasation de la directive structurelle ngFor

--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -10,23 +10,51 @@ export class CarsListComponent implements OnInit {
 
 carUpdate :any
 
-  vehiculeUn : any ={
+
+    cars :any = [
+      {
   name: "Pagani huayra",
     pays: "Italie",
+    coverImage:"./assets/cars/pagani.jpg",
     power: "765",
-    perf: 3.2,};
-    
-  vehiculeDeux : any ={
+    perf: 3.2,},
+
+    {
     name: "Koenigsegg agera rs",
     pays: "Suède",
+    coverImage:"./assets/cars/koenigsegg.jpg",
     power: 1383,
-    perf: 2.6};
+    perf: 2.6},
+   {
+      name: "zenvo tsr s",
+      pays: "Danemark",
+      coverImage:"./assets/cars/zenvo.jpg",
+      power: 1200,
+      perf: 2.8
+    },
+    {
+      name: "bugatti chiron",
+      pays: "France",
+      coverImage:"./assets/cars/bugatti.jpg",
+      power: 1500,
+      perf: 2.4
+    },
+    {
+      name: "mercedes amg one",
+      pays: "Allemagne",
+      coverImage:"./assets/cars/mercedes.jpg",
+      power: 1000,
+      perf: 2.2
+    },
+    {
+      name: "ferrari sf90",
+      pays: "Italie",
+      coverImage:"./assets/cars/ferrari.jpg",
+      power: 1000,
+      perf: 2.5
+    }
 
-  vehiculeTrois : any ={
-    name: "Zenvo tsr s",
-    pays: "Danemark",
-    power: "1200",
-    perf: 2.8};
+    ]
 
   constructor() { }
 
