72e98b27d23005927f4f22959675e8b804c682a5
NabilAbde <80818068+NabilAbde@users.noreply.github.com>
2021-08-05 10:19:54 +0100

    directives structurelle et attribut exemple

--- src/app/drivers-list/drivers-list.component.ts
+++ src/app/drivers-list/drivers-list.component.ts
@@ -9,18 +9,38 @@ export class DriversListComponent implements OnInit {
 
 isOnline:boolean = true;
 
-/*book:any = {
+book:any = {
   title :"Le secret des croisades",
   author : "ThoasAndrea",
   price : -1
 
-}*/
+}
 
 games :string[]= ["Zelda", "Mario", "Outrun"]
+colorOne:string = "blue";
+onOff:any =true;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+getCouleur(){
+  return"green";
+}
 
+onSwitch(){
+  if(this.onOff){
+    this.onOff = false;
+  }
+  else{
+    this.onOff =true;
+  }
+}
+  // condition ? "dans ce cas" : "dans l'autre cas"
+truc(){
+  if (true) 
+    return "dans ce cas"
+  else 
+    return "dans l'autre cas"} 
 }
