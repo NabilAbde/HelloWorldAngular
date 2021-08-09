ca944e89f1757bb82cfa51257ca1beda7d62a7f2
NabilAbde <80818068+NabilAbde@users.noreply.github.com>
2021-08-09 12:06:40 +0100

    affichages pilotes ok

--- src/app/drivers-list/drivers-item/drivers-item.component.ts
+++ src/app/drivers-list/drivers-item/drivers-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-drivers-item',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DriversItemComponent implements OnInit {
 
+  @Input() driver: any // decorateur qui sera appele dans le parent drivers-list <app-drivers-item [driver]="piloteUn"></app-drivers-item>
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+
--- src/app/drivers-list/drivers-list.component.ts
+++ src/app/drivers-list/drivers-list.component.ts
@@ -7,6 +7,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DriversListComponent implements OnInit {
 
+  drivers: any = [
+    {
+      fullName: "ken block",
+      pays: "usa",
+      coverImage: "./assets/drivers/kenblock.jpg",
+      category: "gymkhana",
+      likeIts: -1
+    },
+    {
+      fullName: "danica patrick",
+      pays: "usa",
+      coverImage: "./assets/drivers/danicapatrick.jpg",
+      category: "nascar",
+      likeIts: 0
+    },
+    {
+      fullName: "sebastien loeb",
+      pays: "france",
+      coverImage: "./assets/drivers/Loeb.jpg",
+      category: "rallye",
+      likeIts: 1
+    },
+    {
+      fullName: "molly taylor",
+      pays: "australie",
+      coverImage: "./assets/drivers/mollytaylor.jpg",
+      category: "rallye",
+      likeIts: -1
+    },
+    {
+      fullName: "collin mcrae",
+      pays: "écosse",
+      coverImage: "./assets/drivers/colin.jpg",
+      category: "rallye",
+      likeIts: 0
+    },
+    {
+      fullName: "shirley muldowney",
+      pays: null,
+      coverImage: "./assets/drivers/shirleymuldowney.jpg",
+      category: "drag",
+      likeIts: 0
+    },
+    {
+      fullName: "michael schumacher",
+      pays: "allemagne",
+      coverImage: "./assets/drivers/schumacher.jpg",
+      category: "formule 1",
+      likeIts: 0
+    },
+    {
+      fullName: "erica enders",
+      pays: "usa",
+      coverImage: "./assets/drivers/ericaenders.jpg",
+      category: "drag",
+      likeIts: 0
+    },
+    
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
